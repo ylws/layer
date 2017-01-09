94a57323aedd08eb17a6429013aefa2eadb1f0ef
ylws <474569696@qq.com>
2017-01-09 13:41:43 +0800

    modify bugs

--- global_plugin_win_2.0.js
+++ global_plugin_win_2.0.js
@@ -16,6 +16,8 @@ $.fn.shineonWin = function(options,fahterid)
 	"typewid":"100%",
 	"typehei":"32px",
 	"title":"标题",
+	"iframeid":"",
+	"iframeclass":"iframe",
 	"placehloder":"",
 	"position":"auto",//auto为水平垂直居中，self为自定义传参+positionx,positiony
 	"positionx":"auto",
@@ -31,19 +33,21 @@ $.fn.shineonWin = function(options,fahterid)
 	"timeout":0,//无按钮，几秒后消失
 	"timefn":function(){},//消失后回调
 	"param":"",//弹窗临时调用数据，供iframe内部调用或其他页面调用
+	"templates":{},//弹窗模板临时调用url,data数据，供iframe内部调用或其他页面调用[url,data]
 	"bodyMinWid":1200,//body最小宽度值
+	"iframesubfn":'init',//iframe调用子页面方法
 	},
 	winlen=0,
 	settings  = $.extend({},defaults,options),
 	appendhtml=function(){
 		if($(".win").length!=0)
 		{
-			winlen+=1;
+			winlen=$(".win").length;
 		}
 		var seeheight=document.documentElement.clientHeight;
 		var allhei=document.body.scrollHeight,heis;
 		heis=document.body.scrollTop+seeheight/2-parseInt(settings['height'])/2+"px";
-		var html="<div class=\"win\" id=\"win"+winlen+"\" param=\""+settings['param']+"\">";
+		var html="<div class=\"win\" id=\"win"+winlen+"\" param=\""+settings['param']+"\" >";
 		if(settings['position']=="auto")
 		{
 			html+="<div class=\"win_content\" style=\"height: "+settings['height']+";width:"+settings['width']+";margin:auto\">";
@@ -73,7 +77,11 @@ $.fn.shineonWin = function(options,fahterid)
 			}
 			else if(settings['type']=="iframe")
 			{
-				html+=	'<iframe param="'+settings['param']+'" scrolling="'+settings['scroll']+'" frameborder="0" src="'+settings['typeval']+'" class="iframe" style="margin:'+settings['typemargin']+';padding:'+settings['typepadding']+';width:'+settings['typewid']+';height:'+settings['typehei']+'"/>';
+				if(settings.iframeid!=""){
+					html+=	'<iframe name="iframe'+winlen+'" id="'+settings.iframeid+'" param="'+settings['param']+'" scrolling="'+settings['scroll']+'" frameborder="0" src="'+settings['typeval']+'" class="'+settings.iframeclass+'" style="margin:'+settings['typemargin']+';padding:'+settings['typepadding']+';width:'+settings['typewid']+';height:'+settings['typehei']+'"/>';
+				}else{
+					html+=	'<iframe name="iframe'+winlen+'" param="'+settings['param']+'" scrolling="'+settings['scroll']+'" frameborder="0" src="'+settings['typeval']+'" class="'+settings.iframeclass+'" style="margin:'+settings['typemargin']+';padding:'+settings['typepadding']+';width:'+settings['typewid']+';height:'+settings['typehei']+'"/>';
+				}
 			}
 			else
 			{
@@ -109,6 +117,12 @@ $.fn.shineonWin = function(options,fahterid)
 				$("#win"+winlen).height(allhei);
 			}
 			$("#win"+winlen).show();
+			if(settings['type']=="iframe"){
+				$("#win"+winlen +" .iframe").on("load",function(){
+					var names = "iframe"+winlen;
+					window.frames[names][settings.iframesubfn](settings.templates);
+				})
+			}
 	};
 	var winresizeTimer = null;
 	window.addEventListener("resize",function(){
@@ -138,6 +152,10 @@ $.fn.shineonWin = function(options,fahterid)
 		}, 200);
 	})
 	appendhtml();
+	
+//	if(typeof settings.iframerender === "function"){
+//		settings.iframerender(settings.templates,"win"+winlen)
+//	}
 	if(settings['timeout']!=0)
 	{
 		settings['btnshow']="none";
@@ -153,30 +171,248 @@ $.fn.shineonWin = function(options,fahterid)
 	//点击关闭，隐藏播放窗口
 	var winobj=document.getElementsByClassName("win");
 	for(var i=0;i<winobj.length;i++ ){
-		winobj[i].getElementsByClassName("close")[0].addEventListener("click",function(){
-			var parentid=this.parentNode.parentNode.parentNode.getAttribute("id");
-			settings['close']();
-			$("#"+parentid).remove();
-		},false)
+		if(winobj[i].getElementsByClassName("close").length>0){
+			for(var j=0;j<winobj[i].getElementsByClassName("close").length;j++){//for ie8
+				winobj[i].getElementsByClassName("close")[0].addEventListener("click",function(){
+					var parentid=this.parentNode.parentNode.parentNode.getAttribute("id");
+					settings['close']();
+					$("#"+parentid).remove();
+				},false)
+			}
+		}
+		
 	}
 	//点击确定，隐藏播放窗口
 	for(var i=0;i<winobj.length;i++ ){
 		if(winobj[i].getElementsByClassName("save").length>0){
-			winobj[i].getElementsByClassName("save")[0].addEventListener("click",function(){
-				var parentid=this.parentNode.parentNode.parentNode.getAttribute("id");
-				settings['sure'](parentid);
-			},false)
+			for(var j=0;j<winobj[i].getElementsByClassName("save").length;j++){//for ie8
+				winobj[i].getElementsByClassName("save")[j].addEventListener("click",function(){
+					var parentid=this.parentNode.parentNode.parentNode.getAttribute("id");
+					settings['sure'](parentid);
+				},false)
+			}
 		}
-		
 	}
 	
 	//点击取消，隐藏播放窗口
 	for(var i=0;i<winobj.length;i++ ){
 		if(winobj[i].getElementsByClassName("cancel").length>0){
-			winobj[i].getElementsByClassName("cancel")[0].addEventListener("click",function(){
-				var parentid=this.parentNode.parentNode.parentNode.getAttribute("id");
-				settings['cancel'](parentid);
-			},false)
+			for(var j=0;j<winobj[i].getElementsByClassName("save").length;j++){//for ie8
+				winobj[i].getElementsByClassName("cancel")[j].addEventListener("click",function(){
+					var parentid=this.parentNode.parentNode.parentNode.getAttribute("id");
+					settings['cancel'](parentid);
+				},false)
+			}
+		}
+	}
+	
+	//console.log(settings['btn'].length)
+}
+/*
+ * 名称：弹窗控件/shineonWin
+ * 作者：djl
+ * 邮箱：474569696@qq.com
+ * 日期：2016/4/14
+ */
+$.fn.shineonWin = function(options,fahterid) 
+{			
+	var defaults = {
+	"width":"287px",//弹窗宽度
+	"height":"215px",//弹窗高度
+	"type":"txt",//弹窗类型input/txt/iframe/html
+	"typemargin":"0px",
+	"typepadding":"0px",
+	"typeval":"",//input操作后的值/iframe的src地址
+	"typewid":"100%",
+	"typehei":"32px",
+	"title":"标题",
+	"iframeid":"",
+	"iframeclass":"iframe",
+	"placehloder":"",
+	"position":"auto",//auto为水平垂直居中，self为自定义传参+positionx,positiony
+	"positionx":"auto",
+	"positiony":"auto",
+	"content":'',//嵌入的html代码,文本
+	"btnshow":"block",//按钮显示隐藏block/none
+	"btn":[["cancel","取&nbsp;&nbsp;消"],["save","保&nbsp;&nbsp;存"]],//按钮自定义
+	"headshow":"block",//弹窗头部显示
+	"scroll":"no",//no,yes,auto//iframe滚动与否
+	"sure":function(){},
+	"cancel":function(){},
+	"close":function(){},
+	"timeout":0,//无按钮，几秒后消失
+	"timefn":function(){},//消失后回调
+	"param":"",//弹窗临时调用数据，供iframe内部调用或其他页面调用
+	"templates":{},//弹窗模板临时调用url,data数据，供iframe内部调用或其他页面调用[url,data]
+	"bodyMinWid":1200,//body最小宽度值
+	"iframesubfn":'init',//iframe调用子页面方法
+	},
+	winlen=0,
+	settings  = $.extend({},defaults,options),
+	appendhtml=function(){
+		if($(".win").length!=0)
+		{
+			winlen=$(".win").length;
+		}
+		var seeheight=document.documentElement.clientHeight;
+		var allhei=document.body.scrollHeight,heis;
+		heis=document.body.scrollTop+seeheight/2-parseInt(settings['height'])/2+"px";
+		var html="<div class=\"win\" id=\"win"+winlen+"\" param=\""+settings['param']+"\" >";
+		if(settings['position']=="auto")
+		{
+			html+="<div class=\"win_content\" style=\"height: "+settings['height']+";width:"+settings['width']+";margin:auto\">";
+		}
+		else
+		{//待处理
+			if(window.innerWidth<settings['bodyMinWid']){
+				$("#win"+winlen).width(settings['bodyMinWid']);
+				html+="<div class=\"win_content\" style=\"height: "+settings['height']+";width:"+settings['width']+";margin:auto\">";
+			}else{
+				html+="<div class=\"win_content\" style=\"height: "+settings['height']+";width:"+settings['width']+";left:"+(settings['positionx']=="auto"?($(window).width()/2-parseInt(settings['width'])/2)+"px":settings['positionx'])+";top:"+(settings['positiony']=="auto"?heis:settings['positiony'])+"\">";
+			}
+			
+		}
+			html+=	"<div class=\"title\" style=\"display:"+settings['headshow']+"\">";
+			html+=		"<span class=\"menuname\">"+settings['title']+"</span>";
+			html+=		"<span class=\"close\"></span>";
+			html+=	"</div>";
+			if(settings['type']=="txt")
+			{
+				html+="<div class=\"txt\">"+settings['content']+"</div>";
+			}
+			else if(settings['type']=="input")
+			{
+				// +";padding:"+settings['typepadding']+"
+				html+=	"<input type=\"text\" value=\""+settings['typeval']+"\" class=\"input\" placeholder=\""+settings['placehloder']+"\" style=\"margin:"+settings['typemargin']+";padding:"+settings['typepadding']+";width:"+settings['typewid']+";height:"+settings['typehei']+"\"/>";
+			}
+			else if(settings['type']=="iframe")
+			{
+				if(settings.iframeid!=""){
+					html+=	'<iframe name="iframe'+winlen+'" id="'+settings.iframeid+'" param="'+settings['param']+'" scrolling="'+settings['scroll']+'" frameborder="0" src="'+settings['typeval']+'" class="'+settings.iframeclass+'" style="margin:'+settings['typemargin']+';padding:'+settings['typepadding']+';width:'+settings['typewid']+';height:'+settings['typehei']+'"/>';
+				}else{
+					html+=	'<iframe name="iframe'+winlen+'" param="'+settings['param']+'" scrolling="'+settings['scroll']+'" frameborder="0" src="'+settings['typeval']+'" class="'+settings.iframeclass+'" style="margin:'+settings['typemargin']+';padding:'+settings['typepadding']+';width:'+settings['typewid']+';height:'+settings['typehei']+'"/>';
+				}
+			}
+			else
+			{
+				html+="<div class=\"content\">"+settings['content']+"</div>";
+			}
+			html+= "<div class=\"btnline\"  style=\"display:"+settings['btnshow']+"\">"
+			for(var i =0;i<settings['btn'].length;i++)
+			{
+				if(settings['btn'][i][2]!=undefined){
+					html+=	 "<div class=\""+settings['btn'][i][0]+"\" style=\"margin-left:"+settings['btn'][i][2]+"\">"+settings['btn'][i][1]+"</div>";
+				}
+				else
+				{
+					html+=	 "<div class=\""+settings['btn'][i][0]+"\" >"+settings['btn'][i][1]+"</div>";
+				}
+				
+			}
+			html+= "</div>";
+			html+="</div>";
+			html+="</div>";
+			$("body").append(html);
+			var seeheight=$("body").height();
+			var allhei=document.body.scrollHeight;
+			if(window.innerWidth<settings['bodyMinWid']){
+				$("#win"+winlen).width(settings['bodyMinWid']);
+			}
+			if(allhei<seeheight)
+			{
+				$("#win"+winlen).height(seeheight);
+			}
+			else
+			{
+				$("#win"+winlen).height(allhei);
+			}
+			$("#win"+winlen).show();
+			if(settings['type']=="iframe"){
+				$("#win"+winlen +" .iframe").on("load",function(){
+					var names = "iframe"+winlen;
+					window.frames[names][settings.iframesubfn](settings.templates);
+				})
+			}
+	};
+	var winresizeTimer = null;
+	window.addEventListener("resize",function(){
+		if (winresizeTimer){
+			 clearTimeout(winresizeTimer);
+		}
+		winresizeTimer = setTimeout(function(){
+			var winLen = $(".win").length;
+			if(winLen){
+				var seeheight=$("body").height();
+				var allhei=document.body.scrollHeight;
+				if(window.innerWidth<settings['bodyMinWid']){
+					$(".win").width(settings['bodyMinWid']);
+					$(".win .win_content").css({"left":"0","top":"0","margin":"auto"});
+				}else{
+					$(".win").css("width","100%");
+				}
+				if(allhei<seeheight)
+				{
+					$(".win").height(seeheight);
+				}
+				else
+				{
+					$(".win").height(allhei);
+				}
+			}
+		}, 200);
+	})
+	appendhtml();
+	
+//	if(typeof settings.iframerender === "function"){
+//		settings.iframerender(settings.templates,"win"+winlen)
+//	}
+	if(settings['timeout']!=0)
+	{
+		settings['btnshow']="none";
+		$("#win"+winlen +" .btnline").hide();
+		setTimeout(function(){
+			$("#win"+winlen).remove();
+			if(typeof settings['timefn']==="function"){
+				settings['timefn'](fahterid);
+			}
+		},settings['timeout'])
+		
+	}
+	//点击关闭，隐藏播放窗口
+	var winobj=document.getElementsByClassName("win");
+	for(var i=0;i<winobj.length;i++ ){
+		if(winobj[i].getElementsByClassName("close").length>0){
+			for(var j=0;j<winobj[i].getElementsByClassName("close").length;j++){//for ie8
+				winobj[i].getElementsByClassName("close")[0].addEventListener("click",function(){
+					var parentid=this.parentNode.parentNode.parentNode.getAttribute("id");
+					settings['close']();
+					$("#"+parentid).remove();
+				},false)
+			}
+		}
+		
+	}
+	//点击确定，隐藏播放窗口
+	for(var i=0;i<winobj.length;i++ ){
+		if(winobj[i].getElementsByClassName("save").length>0){
+			for(var j=0;j<winobj[i].getElementsByClassName("save").length;j++){//for ie8
+				winobj[i].getElementsByClassName("save")[j].addEventListener("click",function(){
+					var parentid=this.parentNode.parentNode.parentNode.getAttribute("id");
+					settings['sure'](parentid);
+				},false)
+			}
+		}
+	}
+	
+	//点击取消，隐藏播放窗口
+	for(var i=0;i<winobj.length;i++ ){
+		if(winobj[i].getElementsByClassName("cancel").length>0){
+			for(var j=0;j<winobj[i].getElementsByClassName("save").length;j++){//for ie8
+				winobj[i].getElementsByClassName("cancel")[j].addEventListener("click",function(){
+					var parentid=this.parentNode.parentNode.parentNode.getAttribute("id");
+					settings['cancel'](parentid);
+				},false)
+			}
 		}
 	}
 	
